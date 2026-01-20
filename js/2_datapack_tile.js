dec29db12b70f913454f713a69ce8441abd095e7
NiceKaleido <daskaleidoskop@outlook.de>
2026-01-20 03:26:03 +0100

    fix

--- js/2_datapack_tile.js
+++ js/2_datapack_tile.js
@@ -3,7 +3,7 @@ var datapack2 = [
   "https://modrinth.com/datapack/nice-mob-variants\"><img src=\"uploads/mob_variants.png"  
 , "https://modrinth.com/datapack/nice-keep-inventory\"><img src=\"uploads/keepinv.png"
 , "https://modrinth.com/datapack/nice-admin-tools\"><img src=\"uploads/nat.png"
-, "https://modrinth.com/datapack/nice-name-tags\"><img src=\"uploads/ntt.png"
+, "https://modrinth.com/datapack/nice-name-tags\"><img src=\"uploads/nnt.png"
 , "https://modrinth.com/datapack/nice-villager-master-trades\"><img src=\"uploads/master_trades.png"
 ];
 function getsecondDatapackTag() {
