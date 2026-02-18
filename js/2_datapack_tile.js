96cd9f7f937158c9e89186de9c02f8c07310d4d3
NiceKaleido <daskaleidoskop@outlook.de>
2026-02-18 16:06:11 +0100

    update

--- js/2_datapack_tile.js
+++ js/2_datapack_tile.js
@@ -2,9 +2,8 @@
 var datapack2 = [
   "https://modrinth.com/datapack/nice-mob-variants\"><img src=\"uploads/mob_variants.png"  
 , "https://modrinth.com/datapack/nice-keep-inventory\"><img src=\"uploads/keepinv.png"
-, "https://modrinth.com/datapack/nice-admin-tools\"><img src=\"uploads/nat.png"
+, "https://modrinth.com/datapack/nice-things-eden\"><img src=\"uploads/nice_things.png"
 , "https://modrinth.com/datapack/nice-name-tags\"><img src=\"uploads/nnt.png"
-, "https://modrinth.com/datapack/nice-villager-master-trades\"><img src=\"uploads/master_trades.png"
 ];
 function getsecondDatapackTag() {
 var img = '<a target=\"_blank\" href=\"';
--- js/3_datapack_tile.js
+++ js/3_datapack_tile.js
@@ -2,10 +2,7 @@
 var datapack3 = [
   "https://modrinth.com/datapack/nice-mob-manager\"><img src=\"uploads/mob_manager.png"
 , "https://modrinth.com/datapack/nice-actions\"><img src=\"uploads/actions.png"
-, "https://modrinth.com/datapack/nice-things-eden\"><img src=\"uploads/nice_things.png"
-, "https://modrinth.com/datapack/nice-mobs\"><img src=\"uploads/nice_mobs.png"  
-, "https://modrinth.com/datapack/automaticons\"><img src=\"uploads/am.png"
-
+, "https://modrinth.com/datapack/nice-admin-tools\"><img src=\"uploads/nat.png"
 ];
 function getthirdDatapackTag() {
 var img = '<a target=\"_blank\" href=\"';
